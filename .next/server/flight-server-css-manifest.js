538ae86119ceb3a5737e46c9dab48470a989c254
Daniel Molina <daniel.molina3192@gmail.com>
2023-10-24 14:32:09 -0500

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\page": [
+      "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\globals.css"
+    ],
+    "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\restaurant\\[slug]\\menu\\page": [
+      "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\globals.css"
+    ],
+    "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\restaurant\\[slug]\\page": [
+      "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\globals.css"
+    ]
+  },
+  "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\layout.tsx": [
+    "C:\\Daniel\\Courses\\NextJS13-Bootcamp\\initial-nextjs-app\\app\\globals.css"
+  ]
+}
